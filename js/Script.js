2f76816e14c703171788685b33da7b03f6fd0624
Zucky666 <yammil2015@hotmail.com>
2024-07-04 17:32:17 -0400

    Update Script.js

--- js/Script.js
+++ js/Script.js
@@ -1,8 +1,1 @@
-let counter = 1;
-setInterval(function () {
-    document.getElementById('radio' + counter).checked = true;
-    counter++;
-    if (counter > 7) {
-        counter = 1;
-    }
-}, 2000); // Cambia 5000 a 2000 para que avance cada 2 segundos
+
